14fed233307dd3037afc36eead7ee92e6cf4f1b4
Pieterjan Muller <pjmuller@gmail.com>
2016-10-05 21:23:23 +0200

    tasks json

--- task.go
+++ task.go
@@ -1,6 +1,6 @@
 package podio
 
-import "fmt"
+import "encoding/json"
 
 // https://developers.podio.com/doc/tasks/get-tasks-77949
 func (client *Client) GetTasksJson(params map[string]interface{}) (rawResponse *json.RawMessage, err error) {
