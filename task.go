76dc6b304222dd620eb1bf5fb18893343ce72ddd
Pieterjan Muller <pjmuller@gmail.com>
2016-10-06 07:47:59 +0200

    debugging tasks

--- task.go
+++ task.go
@@ -1,9 +1,13 @@
 package podio
 
 import "encoding/json"
+import "fmt"
 
 // https://developers.podio.com/doc/tasks/get-tasks-77949
 func (client *Client) GetTasksJson(params map[string]interface{}) (rawResponse *json.RawMessage, err error) {
+  fmt.Println("original params", params)
+  params["space"] = 4412774
+  fmt.Println("Sending params", params)
 	err = client.RequestWithParams("GET", "/task/", nil, params, &rawResponse)
 	return
 }
