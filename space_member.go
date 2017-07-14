2855ae932567a85ed05d818f5c4f71718a0db0aa
PJ Muller <pjmuller@gmail.com>
2017-07-14 08:55:28 +0200

    Options argument for find space members

--- space_member.go
+++ space_member.go
@@ -3,12 +3,13 @@ package podio
 import "fmt"
 
 type SpaceMember struct {
-	Profile  Contact  `json:"profile"`
-	Role     string `json:"role"`
+	Profile Contact `json:"profile"`
+	Role    string  `json:"role"`
 }
 
-func (client *Client) FindAllForSpace(id int64) (spaceMembers []SpaceMember, err error) {
+// https://developers.podio.com/doc/space-members/get-space-members-v2-19350328
+func (client *Client) FindAllForSpace(id int64, options map[string]interface{}) (spaceMembers []SpaceMember, err error) {
 	path := fmt.Sprintf("/space/%d/member/v2/", id)
-	err = client.Request("GET", path, nil, nil, &spaceMembers)
+	err = client.RequestWithParams("GET", path, nil, options, &spaceMembers)
 	return
 }
