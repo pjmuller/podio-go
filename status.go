ad8c42e6fde4bde8e103ce6112b6dc1de4cbc3ce
Pieterjan Muller <pjmuller@gmail.com>
2018-09-21 20:41:21 +0200

    status extra methods

--- status.go
+++ status.go
@@ -13,3 +13,15 @@ func (client *Client) StatusCreate(spaceId int64, params map[string]interface{})
 	err = client.RequestWithParams("POST", path, nil, params, &s)
 	return
 }
+
+// https://developers.podio.com/doc/status/update-a-status-message-22338
+func (client *Client) StatusUpdate(statusID int64, params map[string]interface{}) error {
+	path := fmt.Sprintf("/status/%d/", statusID)
+	return client.RequestWithParams("PUT", path, nil, params, nil)
+}
+
+// https://developers.podio.com/doc/status/delete-a-status-message-22339
+func (client *Client) StatusDelete(statusID int64) error {
+	path := fmt.Sprintf("/status/%d/", statusID)
+	return client.Request("DELETE", path, nil, nil, nil)
+}
